6ab7758c4a17073e4e87e92748ce449b9bb64110
VovaTraktorr <117161582+VovaTraktorr@users.noreply.github.com>
2022-11-01 16:39:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app_name = 'textsong'
 token_bot = '5730187466:AAFnMBPmpjTaSsDUETk-Ek9rCEV5e5NvCzg'
 bot = telebot.TeleBot(token_bot)
 
-token = 'oqQOQ34HAOM0Q3UBUFFyjtWCfbUFO-43Lk5xwjrm7novrV8a6M1lBiH7Xx5Mvxgh'
+token_bot = 'oqQOQ34HAOM0Q3UBUFFyjtWCfbUFO-43Lk5xwjrm7novrV8a6M1lBiH7Xx5Mvxgh'
 genius = lyricsgenius.Genius(token)
 
 @bot.message_handler(commands = ['start'])
@@ -24,7 +24,7 @@ def lyrics (msg):
     bot.send_message(msg.chat.id, song.lyrics)
 
 
-@server.route('/' + token, methods=['POST'])
+@server.route('/' + token_bot, methods=['POST'])
 def get_message():
       bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
       return "!", 200
